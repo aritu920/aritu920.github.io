11739006e2668bfc744bd8aaa6efeced3d04225c
aritu920 <afiakuddus.ritu@student.tdsb.on.ca>
2025-01-22 14:33:56 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,25 @@
 
+Conversation opened. 1 read message.
+
+Skip to content
+Using TDSB Mail with screen readers
+4 of 259
+(no subject)
+Inbox
+
+Jimenez, Andrew
+Attachments
+1:49 PM (43 minutes ago)
+to me
+
+
+
+--
+Andrew Jimenez
+Teacher, Birchmount Park C.I.
+416-396-6704
+ 4 Attachments
+  •  Scanned by Gmail
 /**
  * Pose Detection Application
  * Using TensorFlow.js and Teachable Machine
@@ -246,3 +267,5 @@ function stopWebcam() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 }
+newafiascript.txt
+Displaying newafiascript.txt.
